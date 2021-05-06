8e67154d6a2c497a4b372c546963163e3e85b638
leesumin-1 <greenn1@naver.com>
2021-05-07 00:56:23 +0900

    revise is created

--- Korea_birthrate.py
+++ Korea_birthrate.py
@@ -26,23 +26,3 @@ for tr in birthrate_table_tbody_row:
 
 print("")
 
-birthrate_table = soup.find_all('table', {"id":"t_Table_142801"})
-print(len(birthrate_table))
-print(type(birthrate_table))
-print(type(birthrate_table[0]))
-
-birthrate_table_tbody=birthrate_table[0].find_all("thead")
-birthrate_table_tbody_row=birthrate_table_tbody[0].find_all("tr")
-
-birthrate_info=[]
-for tr in birthrate_table_tbody_row:
-    year_birthrate_info=[]
-    td=tr.find_all("th")
-
-    for content in td:
-        print(content.get_text(), end=', ')
-        year_birthrate_info.append(content.get_text())
-    print("")
-    birthrate_info.append(year_birthrate_info)
-
-print("")
